5ca185ef118f5e8478d2d213dd2e9be76dbe34a8
Carlos Holguera <perezholguera@gmail.com>
2025-08-09 16:02:20 +0200

    Refactor watch list in mkdocs.yml and update fix-serve-watch.py to include mastg repo

--- docs/hooks/fix-serve-watch.py
+++ docs/hooks/fix-serve-watch.py
@@ -10,11 +10,16 @@ def on_serve(server, config, **kwargs):
     except ValueError:
         pass
 
-    # Add the masvs and maswe directories to the watch list
-    if config.get('extra', {}).get('maswe_repo'):
-        log.info(f"Watching MASWE repo at {config['extra']['maswe_repo']}")
-        server.watch(config['extra']['maswe_repo'])
+    # Add the mastg, masvs and maswe directories to the watch list
+
+    if config.get('extra', {}).get('mastg_repo'):
+        log.info(f"Watching MASTG repo at {config['extra'].get('mastg_repo')}")
+        server.watch(config['extra']['mastg_repo'])
 
     if config.get('extra', {}).get('masvs_repo'):
         log.info(f"Watching MASVS repo at {config['extra'].get('masvs_repo')}")
         server.watch(config['extra']['masvs_repo'])
+
+    if config.get('extra', {}).get('maswe_repo'):
+        log.info(f"Watching MASWE repo at {config['extra']['maswe_repo']}")
+        server.watch(config['extra']['maswe_repo'])
